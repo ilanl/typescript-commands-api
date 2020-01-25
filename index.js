6e80276e113336c4649a1106a10ae470a48c2655
Ilan Levy <ilan.levy78@gmail.com>
2020-01-25 17:24:13 +0200

    initial from boiler plate

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import routes from './src/routes/crmRoutes';
+
+const app = express();
+
+const PORT = 3000;
+const MONGODB = 'mongodb+srv://admin:wdCRe6NPmRBGzrZp@cluster-qfnvn.mongodb.net/test?retryWrites=true&w=majority'
+
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true , useUnifiedTopology: true })
+  .then(() => { console.log('db okay') })
+// bodyparser setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+routes(app);
+
+// serving static files
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+    res.send(`Node and express server is running on port ${PORT}`)
+);
+
+app.listen(PORT, () =>
+    console.log(`your server is running on port ${PORT}`)
+);
